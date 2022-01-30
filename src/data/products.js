c56e0b2e3f262eaab555e29b8547f339808ae2af
Vladislav Efanov <vladislav.efanov@gmail.com>
2022-01-30 15:09:03 +0100

    add data

---
+++ src/data/products.js
@@ -0,0 +1,88 @@
+export default [
+  {
+    id: 1,
+    title: 'Радионяня Motorola MBP16',
+    price: '3690',
+    image: 'radio.jpg',
+    colors: [
+      '#939393',
+      '#939393',
+      '#939393',
+    ],
+    categoryId: 4,
+  },
+  {
+    id: 2,
+    title: 'Ультразвуковая зубная щётка Playbrush Smart Sonic',
+    price: '5690',
+    image: 'toothbrush.jpg',
+    colors: [
+      '#939393',
+      '#000',
+      '#fff',
+    ],
+    categoryId: 1,
+  },
+  {
+    id: 3,
+    title: 'Смартфон Xiaomi Mi Mix 3 6/128GB',
+    price: '23690',
+    image: 'phone.jpg',
+    colors: [
+      '#939393',
+    ],
+    categoryId: 2,
+  },
+  {
+    id: 4,
+    title: 'Электроскейт Razor Cruiser',
+    price: '24690',
+    image: 'board.jpg',
+    colors: [
+      '#939393',
+    ],
+    categoryId: 3,
+  },
+  {
+    id: 5,
+    title: 'Смартфон Xiaomi Mi A3 4/64GB Android One',
+    price: '14690',
+    image: 'phone-2.jpg',
+    colors: [
+      '#939393',
+    ],
+    categoryId: 2,
+  },
+  {
+    id: 6,
+    title: 'Смартфон Xiaomi Redmi 6/128GB',
+    price: '8960',
+    image: 'phone-3.jpg',
+    colors: [
+      '#939393',
+    ],
+    categoryId: 2,
+  },
+  {
+    id: 7,
+    title: 'Электрический дрифт-карт Razor Crazy Cart',
+    price: '39900',
+    image: 'bicycle.jpg',
+    colors: [
+      '#939393',
+    ],
+    categoryId: 3,
+  },
+  {
+    id: 8,
+    title: 'Гироскутер Razor Hovertrax 2.0',
+    price: '34900',
+    image: 'wheels.jpg',
+    colors: [
+      '#939393',
+      '#000',
+      '#fff',
+    ],
+    categoryId: 3,
+  },
+];
