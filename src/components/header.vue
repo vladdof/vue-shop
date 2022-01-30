3fae7be9e76653bbd93b05373af4bdc346ccb65c
Vladislav Efanov <vladislav.efanov@gmail.com>
2022-01-30 21:36:01 +0100

    fix style of file

--- src/components/header.vue
+++ src/components/header.vue
@@ -4,25 +4,26 @@
       <span class="header__info">Каталог</span>
 
       <a class="header__logo" href="#">
-        <img src="img/svg/logo-tech.svg" alt="Логотип интернет магазина Технозавррр" width="190" height="33">
+        <img src="img/svg/logo-tech.svg"
+          alt="Логотип интернет магазина Технозавррр" width="190" height="33">
       </a>
 
       <a class="header__tel" href="tel:8 800 600 90 09">
         8 800 600 90 09
       </a>
 
-      <a class="header__cart" href="cart.html" aria-label="Корзина с товарами">
-        <svg width="30" height="21" fill="currentColor">
-          <use xlink:href="#icon-cart"></use>
-        </svg>
-        <span class="header__count" aria-label="Количество товаров">3</span>
-      </a>
+      <cart-indicator />
     </div>
   </header>
 </template>
 
 <script>
+import CartIndicator from '@/components/cart-indicator.vue';
+
 export default {
   name: 'header',
+  components: {
+    CartIndicator,
+  },
 };
 </script>
