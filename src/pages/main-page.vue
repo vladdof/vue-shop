1f5c25a6b8ed59d9fb2ae734dcaba9c8ce8f7192
Vladislav Efanov <vladislav.efanov@gmail.com>
2022-01-30 15:08:44 +0100

    add pages

---
+++ src/pages/main-page.vue
@@ -0,0 +1,92 @@
+<template>
+  <main class="content container">
+    <div class="content__top content__top--catalog">
+      <h1 class="content__title">
+        Каталог
+      </h1>
+      <span class="content__info">
+        152 товара
+      </span>
+    </div>
+
+    <div class="content__catalog">
+      <product-filter
+        :price-from.sync="filterPriceFrom"
+        :price-to.sync="filterPriceTo"
+        :category-id.sync="filterCategoryId"
+        :color.sync="filterColor"
+      />
+
+      <section class="catalog">
+        <product-list :products="products" />
+        <pagination
+          v-model="page"
+          :count="getProductsLength"
+          :per-page="productsPerPage"
+        />
+      </section>
+    </div>
+  </main>
+</template>
+
+<script>
+import products from '../data/products';
+import Pagination from '../components/pagination/pagination.vue';
+import ProductList from '../components/product-list/product-list.vue';
+import ProductFilter from '../components/product-filter/product-filter.vue';
+
+export default {
+  name: 'main-page',
+  components: {
+    Pagination,
+    ProductList,
+    ProductFilter,
+  },
+  data() {
+    return {
+      page: 1,
+      productsPerPage: 3,
+      filterPriceFrom: 0,
+      filterPriceTo: 0,
+      filterCategoryId: 0,
+      filterColor: null,
+    };
+  },
+  computed: {
+    products() {
+      // start for slice
+      const offset = (this.page - 1) * this.productsPerPage;
+
+      return this.filterProducts.slice(offset, offset + this.productsPerPage);
+    },
+    getProductsLength() {
+      return this.filterProducts.length;
+    },
+    filterProducts() {
+      let filterProducts = products;
+
+      if (this.filterPriceFrom > 0) {
+        filterProducts = filterProducts
+          .filter((product) => product.price > this.filterPriceFrom);
+      }
+
+      if (this.filterPriceTo > 0) {
+        filterProducts = filterProducts
+          .filter((product) => product.price > this.filterPriceTo);
+      }
+
+      if (this.filterCategoryId > 0) {
+        filterProducts = filterProducts
+          .filter((product) => product.categoryId === this.filterCategoryId);
+      }
+
+      if (this.filterColor) {
+        filterProducts = filterProducts
+          .filter((product) => product.colors.includes(this.filterColor.toLowerCase()));
+      }
+
+      return filterProducts;
+    },
+  },
+};
+</script>
---
+++ src/pages/product-page.vue
@@ -0,0 +1,321 @@
+<template>
+  <main class="content container">
+    <div class="content__top">
+      <ul class="breadcrumbs">
+        <li class="breadcrumbs__item">
+          <a
+            class="breadcrumbs__link"
+            href="#"
+            @click.prevent="goToPage('main')"
+          >
+            Каталог
+          </a>
+        </li>
+        <li class="breadcrumbs__item">
+          <a
+            class="breadcrumbs__link"
+            href="#"
+            @click.prevent="goToPage('main')"
+          >
+            {{ category.title }}
+          </a>
+        </li>
+        <li class="breadcrumbs__item">
+          <a class="breadcrumbs__link">
+            {{ product.title }}
+          </a>
+        </li>
+      </ul>
+    </div>
+
+    <section class="item">
+      <div class="item__pics pics">
+        <div class="pics__wrapper">
+          <img
+            width="570"
+            height="570"
+            :src="`img/${product.image}`"
+            :alt="product.title"
+          >
+        </div>
+      </div>
+
+      <div class="item__info">
+        <span class="item__code">Артикул: {{ product.id }}</span>
+        <h2 class="item__title">
+          {{ product.title }}
+        </h2>
+        <div class="item__form">
+          <form
+            class="form"
+            action="#"
+            method="POST"
+          >
+            <b class="item__price">
+              {{ product.price | numberFormat }} ₽
+            </b>
+
+            <fieldset class="form__block">
+              <legend class="form__legend">
+                Цвет:
+              </legend>
+              <ul class="colors">
+                <li class="colors__item">
+                  <label class="colors__label">
+                    <input
+                      class="colors__radio sr-only"
+                      type="radio"
+                      name="color-item"
+                      value="blue"
+                      checked=""
+                    >
+                    <span
+                      class="colors__value"
+                      style="background-color: #73B6EA;"
+                    />
+                  </label>
+                </li>
+                <li class="colors__item">
+                  <label class="colors__label">
+                    <input
+                      class="colors__radio sr-only"
+                      type="radio"
+                      name="color-item"
+                      value="yellow"
+                    >
+                    <span
+                      class="colors__value"
+                      style="background-color: #FFBE15;"
+                    />
+                  </label>
+                </li>
+                <li class="colors__item">
+                  <label class="colors__label">
+                    <input
+                      class="colors__radio sr-only"
+                      type="radio"
+                      name="color-item"
+                      value="gray"
+                    >
+                    <span
+                      class="colors__value"
+                      style="background-color: #939393;"
+                    /></label>
+                </li>
+              </ul>
+            </fieldset>
+
+            <fieldset class="form__block">
+              <legend class="form__legend">
+                Объемб в ГБ:
+              </legend>
+
+              <ul class="sizes sizes--primery">
+                <li class="sizes__item">
+                  <label class="sizes__label">
+                    <input
+                      class="sizes__radio sr-only"
+                      type="radio"
+                      name="sizes-item"
+                      value="32"
+                    >
+                    <span class="sizes__value">
+                      32gb
+                    </span>
+                  </label>
+                </li>
+                <li class="sizes__item">
+                  <label class="sizes__label">
+                    <input
+                      class="sizes__radio sr-only"
+                      type="radio"
+                      name="sizes-item"
+                      value="64"
+                    >
+                    <span class="sizes__value">
+                      64gb
+                    </span>
+                  </label>
+                </li>
+                <li class="sizes__item">
+                  <label class="sizes__label">
+                    <input
+                      class="sizes__radio sr-only"
+                      type="radio"
+                      name="sizes-item"
+                      value="128"
+                      checked=""
+                    >
+                    <span class="sizes__value">
+                      128gb
+                    </span>
+                  </label>
+                </li>
+              </ul>
+            </fieldset>
+
+            <div class="item__row">
+              <div class="form__counter">
+                <button
+                  type="button"
+                  aria-label="Убрать один товар"
+                >
+                  <svg
+                    width="12"
+                    height="12"
+                    fill="currentColor"
+                  >
+                    <use xlink:href="#icon-minus" />
+                  </svg>
+                </button>
+
+                <input
+                  type="text"
+                  value="1"
+                  name="count"
+                >
+
+                <button
+                  type="button"
+                  aria-label="Добавить один товар"
+                >
+                  <svg
+                    width="12"
+                    height="12"
+                    fill="currentColor"
+                  >
+                    <use xlink:href="#icon-plus" />
+                  </svg>
+                </button>
+              </div>
+
+              <button
+                class="button button--primery"
+                type="submit"
+              >
+                В корзину
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+
+      <div class="item__desc">
+        <ul class="tabs">
+          <li class="tabs__item">
+            <a class="tabs__link tabs__link--current">
+              Описание
+            </a>
+          </li>
+          <li class="tabs__item">
+            <a
+              class="tabs__link"
+              href="#"
+            >
+              Характеристики
+            </a>
+          </li>
+          <li class="tabs__item">
+            <a
+              class="tabs__link"
+              href="#"
+            >
+              Гарантия
+            </a>
+          </li>
+          <li class="tabs__item">
+            <a
+              class="tabs__link"
+              href="#"
+            >
+              Оплата и доставка
+            </a>
+          </li>
+        </ul>
+
+        <div class="item__content">
+          <p>
+            Навигация GPS, ГЛОНАСС, BEIDOU Galileo и QZSS<br>
+            Синхронизация со смартфоном<br>
+            Связь по Bluetooth Smart, ANT+ и Wi-Fi<br>
+            Поддержка сторонних приложений<br>
+          </p>
+
+          <a href="#">
+            Все характеристики
+          </a>
+
+          <h3>Что это?</h3>
+
+          <p>
+            Wahoo ELEMNT BOLT GPS – это велокомпьютер,
+            который позволяет оптимизировать свои
+            велотренировки, сделав их максимально
+            эффективными. Wahoo ELEMNT BOLT GPS
+            синхронизируется с датчиками по ANT+,
+            объединяя полученную с них информацию.
+            Данные отображаются на дисплее, а также
+            сохраняются на смартфоне. При этом на
+            мобильное устройство можно установить
+            как фирменное приложение, так и различные
+            приложения сторонних разработчиков.
+            Велокомпьютер точно отслеживает местоположение,
+            принимая сигнал с целого комплекса спутников.
+            Эта информация позволяет смотреть уже преодоленные
+            маршруты и планировать новые велопрогулки.
+          </p>
+
+          <h3>Дизайн</h3>
+
+          <p>
+            Велокомпьютер Wahoo ELEMNT BOLT очень
+            компактный. Размеры устройства составляют
+            всего 74,6 x 47,3 x 22,1 мм. что не
+            превышает габариты смартфона. Корпус
+            гаджета выполнен из черного пластика.
+            На обращенной к пользователю стороне
+            расположен дисплей диагональю 56 мм.
+            На дисплей выводятся координаты и
+            скорость, а также полученная со
+            смартфона и синхронизированных датчиков
+            информация: интенсивность, скорость
+            вращения педалей, пульс и т.д. (датчики
+            не входят в комплект поставки). Корпус
+            велокомпьютера имеет степень защиты
+            от влаги IPX7. Это означает, что
+            устройство не боится пыли, а также
+            выдерживает кратковременное (до 30
+            минут) погружение в воду на глубину
+            не более 1 метра.
+          </p>
+        </div>
+      </div>
+    </section>
+  </main>
+</template>
+
+<script>
+import products from '@/data/products';
+import categories from '@/data/categories';
+import goToPage from '@/helpers/goToPage';
+import numberFormat from '@/helpers/numberFormat';
+
+export default {
+  name: 'product-page',
+  props: ['pageParams'],
+  filters: {
+    numberFormat,
+  },
+  computed: {
+    product() {
+      return products.find((product) => product.id === this.pageParams.id);
+    },
+    category() {
+      return categories.find((category) => category.id === this.product.categoryId);
+    },
+  },
+  methods: {
+    goToPage,
+  },
+};
+</script>
