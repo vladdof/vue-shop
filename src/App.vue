622830c8d8051f6286d123a1c8993cc021d5b70c
Vladislav Efanov <vladislav.efanov@gmail.com>
2022-01-30 21:37:23 +0100

    change layout of App

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,20 @@
 <template>
-  <component
-    :is="currentPageComponent"
-    :page-params="currentPageParams"
-  />
+  <div>
+    <header-cmp />
+    <router-view />
+    <footer-cmp />
+  </div>
 </template>
 
 <script>
-import eventBus from '@/eventBus';
-import MainPage from '@/pages/main-page.vue';
-import ProductPage from '@/pages/product-page.vue';
-import NotFoundPage from '@/pages/not-found-page.vue';
-
-const routes = {
-  main: 'main-page',
-  product: 'product-page',
-  default: 'not-found-page',
-};
+import HeaderCmp from '@/components/header.vue';
+import FooterCmp from '@/components/footer.vue';
 
 export default {
   name: 'App',
   components: {
-    MainPage,
-    ProductPage,
-    NotFoundPage,
-  },
-  data() {
-    return {
-      currentPage: 'main',
-      currentPageParams: {
-
-      },
-    };
-  },
-  created() {
-    eventBus.$on('goToPage', (pageName, pageParams) => this.goToPage(pageName, pageParams));
-  },
-  computed: {
-    currentPageComponent() {
-      return routes[this.currentPage] || routes.default;
-    },
-  },
-  methods: {
-    goToPage(pageName, pageParams) {
-      console.log(pageName, pageParams);
-      this.currentPage = pageName;
-      this.currentPageParams = pageParams || {};
-    },
+    HeaderCmp,
+    FooterCmp,
   },
 };
 </script>
