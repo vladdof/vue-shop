bb1b3a51db0725429878fb5b088f3ced0a10d099
Vladislav Efanov <vladislav.efanov@gmail.com>
2022-01-30 21:34:46 +0100

    Cart of components

---
+++ src/components/cart-indicator.vue
@@ -0,0 +1,20 @@
+<template>
+  <router-link
+    class="header__cart"
+    aria-label="Корзина с товарами"
+    :to="{ name: 'cart' }"
+  >
+    <svg width="30" height="21" fill="currentColor">
+      <use xlink:href="#icon-cart"></use>
+    </svg>
+    <span class="header__count" aria-label="Количество товаров">
+      {{ $store.state.cartProducts.length }}
+    </span>
+  </router-link>
+</template>
+
+<script>
+export default {
+  name: 'cart-indicator',
+};
+</script>
---
+++ src/pages/cart-page.vue
@@ -0,0 +1,74 @@
+<template>
+  <main class="content container">
+    <div class="content__top">
+      <ul class="breadcrumbs">
+        <li class="breadcrumbs__item">
+          <a class="breadcrumbs__link" href="index.html">
+            Каталог
+          </a>
+        </li>
+        <li class="breadcrumbs__item">
+          <a class="breadcrumbs__link">
+            Корзина
+          </a>
+        </li>
+      </ul>
+
+      <h1 class="content__title">
+        Корзина
+      </h1>
+      <span class="content__info">
+        3 товара
+      </span>
+    </div>
+
+    <section class="cart">
+      <form class="cart__form form" action="#" method="POST">
+        <div class="cart__field">
+          <ul class="cart__list">
+            <cart-item
+              v-for="item in products"
+              :key="item.productId"
+              :item="item"
+            />
+          </ul>
+        </div>
+
+        <div class="cart__block">
+          <p class="cart__desc">
+            Мы&nbsp;посчитаем стоимость доставки на&nbsp;следующем этапе
+          </p>
+          <p class="cart__price">
+            Итого: <span>{{ totalPrice | numberFormat }} ₽</span>
+          </p>
+
+          <button class="cart__button button button--primery" type="submit">
+            Оформить заказ
+          </button>
+        </div>
+      </form>
+    </section>
+  </main>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+import numberFormat from '@/helpers/numberFormat';
+import { CartItem } from '@/components/cart-item.vue';
+
+export default {
+  name: 'cart-page',
+  components: {
+    CartItem,
+  },
+  filters: {
+    numberFormat,
+  },
+  computed: {
+    ...mapGetters({
+      products: 'cartDetailProducts',
+      totalPrice: 'cartTotalPrice',
+    }),
+  },
+};
+</script>
