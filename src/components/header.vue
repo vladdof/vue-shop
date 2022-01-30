3feba4a521ed1a2cb48c449d12affb4941caa149
Vladislav Efanov <vladislav.efanov@gmail.com>
2022-01-30 15:31:31 +0100

    add footer and header cmp

---
+++ src/components/header.vue
@@ -0,0 +1,28 @@
+<template>
+  <header class="header">
+    <div class="header__wrapper container">
+      <span class="header__info">Каталог</span>
+
+      <a class="header__logo" href="#">
+        <img src="img/svg/logo-tech.svg" alt="Логотип интернет магазина Технозавррр" width="190" height="33">
+      </a>
+
+      <a class="header__tel" href="tel:8 800 600 90 09">
+        8 800 600 90 09
+      </a>
+
+      <a class="header__cart" href="cart.html" aria-label="Корзина с товарами">
+        <svg width="30" height="21" fill="currentColor">
+          <use xlink:href="#icon-cart"></use>
+        </svg>
+        <span class="header__count" aria-label="Количество товаров">3</span>
+      </a>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'header',
+};
+</script>
